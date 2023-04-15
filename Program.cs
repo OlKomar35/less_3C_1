aba07a1e7d3dcfea6ae81a6d2c7b2d35e5438d2e
Olga Komar <komar_ol@mail.ru>
2023-04-15 19:37:02 +0300

    added the solution to the first task

--- Program.cs
+++ Program.cs
@@ -5,3 +5,53 @@
 //12821 -> да
 //23432 -> да
 
+int FiveDigitNumber()
+{
+    bool isSelector = true;
+    int num = 0;
+    while (isSelector)
+    {
+        Console.Write("Ввудите пятизначное число = ");
+        num = Convert.ToInt32(Console.ReadLine());
+        if (num >= 10000 && num < 100000)
+        {
+            Console.WriteLine($"Введенное число = {num}");
+            isSelector = false;
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"Введенное число {num} не является пятизначным, попробуйте снова");
+        }
+    }
+    return num;
+}
+
+
+int number = FiveDigitNumber();
+
+int numFirst, numLast;
+int divFirst = 10000;
+
+while (number > 0)
+{
+    if (number < 10)
+    {
+        Console.WriteLine("Данное число полиндром");
+        break;
+    }
+    else
+    {
+        numFirst = number / divFirst;
+        numLast = number % 10;
+        if (numFirst == numLast)
+        {
+            number = number % divFirst / 10;
+            divFirst = divFirst / 100;
+        }else{
+            Console.WriteLine("Данное число не полиндром");
+            break;
+        }
+    }
+}
+
